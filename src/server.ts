d84a1ee02db374a00a67ffa17fe479634650a6b8
aldi <aldiposhnjari@gmail.com>
2026-07-12 22:33:00 +0200

    feat: update output path for Angular build and enhance SSR entry point export

--- src/server.ts
+++ src/server.ts
@@ -105,6 +105,9 @@ if (isMainModule(import.meta.url) || process.env['pm_id']) {
 }
 
 /**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
+ * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud
+ * Functions. Also exported as default since that's the convention platforms like Vercel look for
+ * to detect the SSR entry point.
  */
 export const reqHandler = createNodeRequestHandler(app);
+export default reqHandler;
